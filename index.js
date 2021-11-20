37115add489a9680f3e56d3817e96c27108a2d39
James Threadgill <calvinthreadgill01@gmail.com>
2021-11-19 21:21:20 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(James) {
+    cats.push (James);
+}
+
+function destructivelyPrependCat(Dakota){
+    cats.unshift(Dakota);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(Calvin){
+    return [...cats, Calvin];
+}
+
+function prependCat(Arnold){
+    return [Arnold, ...cats];
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
+
+function removeLastCat(){
+    return cats.slice(0, cats.length -1);
+}
